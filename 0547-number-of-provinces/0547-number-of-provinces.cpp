2fdfe8c7c5c51ae1bf6001a21a179d09b6299477
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2024-12-18 14:28:18 +0530

    Time: 0 ms (100%), Space: 19.4 MB (59.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,50 +1,41 @@
-class DisjointSet
-{
+class Disjoint{
 public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n){
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++){
+    vector<int>parent,size,rank; 
+    Disjoint(int n){
+        size.resize(n+1,1);
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
             parent[i] = i;
         }
     }
-    int findPar(int node){
-        if (node == parent[node]) return node;
-        return  findPar(parent[node]); // path compression.
+    int findpar(int node){
+        if(parent[node]==node) return node;
+        return parent[node] = findpar(parent[node]);
     }
-    void unionByRank(int u, int v){
-        int ulp_u = findPar(u);
-        int ulp_v = findPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        // connect the smallest one to the largest one...
-        if (rank[ulp_u] < rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-        else{
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
+    void unionbyrank(int u,int v){
+        int up = findpar(u);
+        int vp = findpar(v);
+        if(up==vp) return ;
+        if(rank[up]<rank[vp]){
+            parent[up] = vp;
+        }else if(rank[up]>rank[vp]){
+            parent[vp] = up;
+        }else{
+            parent[vp] =up;
+            rank[up]++;
         }
     }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findPar(u);
-        int ulp_v = findPar(v);
-        if (ulp_u == ulp_v)
-            return;
-        // connect the smallest one to the largest one...
-        if (size[ulp_u] < size[ulp_v]){
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+    void unionbysize(int u,int v){
+        int up = findpar(u);
+        int vp = findpar(v);
+        if(up==vp) return ;
+        if(size[up]<size[vp]){
+            parent[up] = vp;
+            size[vp]+=size[up];
+        }else{
+            parent[vp] = up;
+            size[up]+=size[vp];
         }
     }
 };
@@ -52,12 +43,12 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int v = isConnected.size();
-        DisjointSet ds(v);
-        vector<int>adj[v];
-        for(int i=0;i<v;i++){
+        Disjoint ds(v);
+        vector<int>adj(v);
+       for(int i=0;i<v;i++){
             for(int j=0;j<v;j++){
                 if(isConnected[i][j]==1){
-                    ds.unionBySize(i,j);
+                    ds.unionbysize(i,j);
                 } 
             }
         }
