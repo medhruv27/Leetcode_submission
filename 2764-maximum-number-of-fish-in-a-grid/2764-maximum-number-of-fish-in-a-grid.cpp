6b2fb8fdd5aa6eca2b40f24551246e8d79e3d1e3
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2024-12-19 08:08:42 +0530

    Time: 20 ms (28.42%), Space: 110 MB (19.38%) - LeetHub

---
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+int bfs(int r ,int c, vector<vector<int>>&vis , vector<vector<int>>&grid,int cnt){
+        queue<pair<int,int>>q;
+        int drow[] ={-1,0,1,0};
+        int dcol[] ={0,1,0,-1};
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[r][c]=1;
+        cnt+=grid[r][c];
+        q.push({r,c});
+        while(!q.empty()){
+             int row = q.front().first;
+             int col = q.front().second;
+             q.pop();
+             for(int i=0;i<4;i++){
+                    int rn = row + drow[i];
+                    int cn = col + dcol[i];
+                    if( rn>=0 &&rn<n && cn>=0 && cn<m && !vis[rn][cn] && grid[rn][cn]>= 1 ){
+                        vis[rn][cn] =1;
+                        cnt+=grid[rn][cn];
+                        q.push({rn,cn});
+                    }
+                
+            }
+        }
+        return cnt;
+      
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int cmax=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]>= 1){
+                    int c = bfs(i,j,vis,grid,0);
+                    cmax = max(c,cmax);
+                }
+            }
+        }
+        return cmax;
+    }
+};
