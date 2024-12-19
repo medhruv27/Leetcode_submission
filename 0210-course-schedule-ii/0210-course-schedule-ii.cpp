25359e1d8661fe57871ef3f324cceca023b928eb
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2024-12-20 04:07:05 +0530

    Time: 9 ms (22.38%), Space: 17.3 MB (89.27%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<int>adj[V];
+        for(auto it :prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>indeg(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it : adj[i]){
+                indeg[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        vector<int>topo;
+        while(!q.empty()){
+            int node  = q.front();
+            q.pop();
+            c++;
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(c==V)
+        return topo;
+        else
+        return {};
+    }
+};
