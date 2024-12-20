7ff56588d040b8f80fe53c3a5d26974d16394547
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2024-12-21 03:33:32 +0530

    Time: 51 ms (76.7%), Space: 24.6 MB (41.2%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -4,7 +4,9 @@ public:
         int n  = heights.size();
         int m = heights[0].size();
         vector<vector<int>>dis(n,vector<int>(m,1e9));
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        priority_queue<pair<int,pair<int,int>>, 
+                       vector<pair<int,pair<int,int>>>, 
+                       greater<pair<int,pair<int,int>>>> pq;
         int drow[]= {-1,0,1,0};
         int dcol[]= {0,1,0,-1};
         pq.push({0,{0,0}});
