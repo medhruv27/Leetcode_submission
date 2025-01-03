3d9c0efdf93259e55507438dc58e3d3ca1e9240f
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-01-04 03:03:03 +0530

    Time: 44 ms (22.08%), Space: 92.1 MB (41.85%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -0,0 +1,25 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> st;
+    int i = 0;
+    int nglindex;
+    StockSpanner() {}
+    int next(int price) {
+        if (st.empty()) {
+            nglindex = -1;
+        } else {
+            while (!st.empty() && st.top().first <= price) {
+                st.pop();
+            }
+            if (st.empty()) {
+                nglindex = -1;
+            } else {
+                nglindex = st.top().second;
+            }
+        }
+        st.push({price, i});
+        int span = i - nglindex;
+        i++;
+        return span;
+    }
+};
