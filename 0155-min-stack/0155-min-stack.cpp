42b343a977f155b2e18e4fa4f531da8703fa7141
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-01-04 04:31:42 +0530

    Time: 3 ms (63.84%), Space: 23.6 MB (20.65%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,35 +1,68 @@
 class MinStack {
 public:
-    stack<int> s;
-    stack<int> ss;
-    MinStack() {
-        
-    }
-    
+    stack<long long> s;
+    long long min;
+    MinStack() {}
     void push(int val) {
-        s.push(val);
-        if(ss.size()==0 || val<=ss.top()){
-            ss.push(val);
+        if (s.size() == 0) {
+            s.push(val);
+            min = val;
+
+        } else {
+            if (val >= min) {
+                s.push(val);
+            }
+            else // if (val<min)
+            {   // s.push(2*val-min);this line is causing integer overflow error
+                // so to remove it we have something special to remember for the
+                // lifetime lessons
+                long long temp = val;
+                temp = temp * 2;
+                s.push(temp - min);
+                min = val;
+            }
         }
     }
-    
-    void pop() {
-        //if(s.size()==0) return -1; iski koi need nahi hai 
-        int ans = s.top();
-        s.pop();
 
-        if(ss.top()==ans)
-        ss.pop();
+    void pop() {           // pop ko samjhana is very  very important
+        if (s.size() == 0) // you cant do return -1 in void function
+        {
+            //  return -1;
+        } else {
+            if (s.top() >= min)
+                s.pop();
+
+            else // if(s.top()<min)
+            {
+
+                min = 2 * min - s.top();
+                s.pop();
+            }
+        }
     }
-    
-    int top() {
-        //if(s.size()==0) return -1; iski koi need nahi hai
-        return s.top();
+
+    int top() { // also top ko samjhana is very very important
+        if (s.size() == 0) {
+            return -1;
+
+        } else {
+
+            if (s.top() >= min)
+                return s.top();
+            else // if(s.top()<min)
+            {
+                return min;
+            }
+        }
+        return -1; // i added this myself to make the code pass becoz an int
+                   // function must return something
     }
-    
+
     int getMin() {
-        //if(ss.size()==0) return -1; iski koi need nahi hai
-        return ss.top();
+        if (s.size() == 0) {
+            return -1;
+        }
+        return min;
     }
 };
 
