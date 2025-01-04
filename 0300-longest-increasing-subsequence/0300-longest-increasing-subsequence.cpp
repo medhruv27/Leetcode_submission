1dad1ebfc319c80228283c0092e78bc8b7bc5182
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-01-04 14:01:39 +0530

    Time: 555 ms (17.23%), Space: 296.5 MB (7.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int index,int prev,int n,vector<vector<int>>&dp,vector<int>nums){
+    int solve(int index,int prev,int n,vector<vector<int>>&dp,vector<int>&nums){
         if(index==n) return 0;
         if(dp[index][prev+1]!=-1) return dp[index][prev+1];
         int len = solve(index+1,prev,n,dp,nums);
@@ -12,7 +12,7 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         return solve(0,-1,n,dp,nums);
     }
 };
