7606a154ae3e592d842fef3894006b9524da4a53
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-01-07 20:17:15 +0530

    Time: 0 ms (100%), Space: 10.9 MB (51.8%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,30 +12,25 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* curr= root;
         vector<int>morris;
-        TreeNode* curr = root;
         while(curr!=NULL){
             if(curr->left==NULL){
-                morris.push_back(curr->val);
-                curr = curr->right;// 7 ka right 1 fir 4 ka right 2
-            }
-            else{
-                TreeNode* prev = curr->left;
-                while(prev->right!=NULL && prev->right!=curr){
-                    prev= prev->right;//  7 tak jane kai liye // fir 4 tak jane kai liye 
-                }
-                if(prev->right==NULL){
-                    prev->right = curr; //first time thread ban rha jodo fir aage badho 
-                    curr = curr->left;
-                }
-                else if(prev->right==curr){
-                    prev->right=NULL; // ab 2 and 4 vala hoga jisme 2 ka left bhi 
-                                       // 4(prev) hai aur 4 ka right bhi 2 hai  
-                    morris.push_back(curr->val); // iska matlab tum root pe ho root ko push kiya fir curr kai right gaye fir ye similar chiz kiya
-                    curr = curr->right;
-                    
-                }
-                
+               morris.push_back(curr->val);
+               curr = curr->right;
+            }else{
+               TreeNode* prev = curr->left;
+               while(prev->right && prev->right!=curr){
+                 prev = prev ->right;
+               }
+               if(prev->right==NULL){
+                  prev->right=curr;
+                  curr= curr->left;
+               }else if(prev->right==curr){
+                  prev->right = NULL;
+                  morris.push_back(curr->val);
+                  curr = curr->right;
+               }
             }
         }
         return morris;
