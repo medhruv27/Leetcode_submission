84544e5270575ea8a393595ba8db5f204a487129
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-01-13 16:05:13 +0530

    Time: 2640 ms (5%), Space: 718.4 MB (5.03%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,9 +1,35 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<vector<int>>&dp,vector<int>coins){
+        if(i==0) return INT_MAX-1;
+        if(j==0) return 0;
+        if(i==1){
+            if(j%coins[i-1] ==0){
+                 return j/coins[0];
+            }
+            else{
+                 return INT_MAX-1;
+            }
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(coins[i-1] <=j){
+                    dp[i][j]= min(solve(i,j-coins[i-1],dp,coins) +1,solve(i-1,j,dp,coins));
+                }
+                else{
+                    dp[i][j] = solve(i-1,j,dp,coins);
+                }
+                return dp[i][j];
+
+    }
     int coinChange(vector<int>& coins, int w) {
         int n = coins.size();
-        int dp[n+1][w+1];
-        memset(dp,-1,sizeof(dp));
+        vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+        int ans =  solve(n,w,dp,coins);
+        if(ans>= INT_MAX-1){
+            return -1;
+        }
+        return ans;
+
         for(int i=0;i<n+1;i++){
             for(int j=0;j<w+1;j++){
                 if(j==0){
