6ee1714293cc1ac80b3bb0481c6d49f0dda74897
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-10-24 02:45:11 +0530

    Time: 6 ms (33.49%), Space: 102.4 MB (43.94%) - LeetHub

---
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        vector<int> mn(nums.size(), nums.back());
+        for(int i = nums.size() - 2;i >= 0;i--) mn[i] = min(nums[i], mn[i + 1]);
+        
+        int mx = 0;
+        for(int i = 0;i < nums.size();i++){
+            mx = max(mx, nums[i]);
+            if(mx <= mn[i + 1]) return i + 1;
+        }
+
+        return 0;
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
