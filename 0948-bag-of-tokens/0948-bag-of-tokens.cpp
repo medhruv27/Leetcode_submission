fc0bf89c703a26822188ffe7cfa15ec6c68103a0
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-01 00:30:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& t, int p) {
+        sort(t.begin(),t.end());
+        int s=0,e=t.size()-1,c=0;
+        while(s<e){
+            if(c==0 || t[s]<=p){ //faceup
+                if(t[s]<p){
+                    p-=t[s];
+                    c++;
+                    cout<<c<<p;
+                }
+                s++;
+            }else if(p<t[s] && c>=1){
+                p += t[e];
+                c--;
+                e--;
+                cout<<c;
+            }
+        }
+        if(s==e){
+            if(t[s]<=p)c++;
+        }
+        return c;
+    }
+};
