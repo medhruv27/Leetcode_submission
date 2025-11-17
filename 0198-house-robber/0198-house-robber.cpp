ea5b971f5a659a8be76ccd82031724a1c5501d67
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-18 01:30:37 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,7 +7,7 @@ public:
         dp[0] = nums[0];
         dp[1] = max(dp[0],nums[1]);
         for(int i=2;i<n;i++){
-            dp[i] = max(dp[i-1] , nums[i] + dp[i-2]);
+            dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
         }
         return dp[n-1];
     }
