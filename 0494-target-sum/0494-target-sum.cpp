62b7f43a498b91d42de09ecf5ff07075cc6768e9
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-20 15:53:04 +0530

    Time: 341 ms (28.75%), Space: 11.7 MB (83.22%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int n,int w,vector<int>&nums){
+        if(w==0 && n==0) return 1;
+        if(n==0){
+            return 0;
+        }
+        
+        if(nums[n-1]<=w){
+            return solve(n-1,w-nums[n-1],nums) + solve(n-1,w,nums);
+        }
+        else{
+            return solve(n-1,w,nums);
+        }
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum  = accumulate(nums.begin(),nums.end(),0);
+        target = abs(target);
+        if (sum < target || (target + sum) % 2 != 0) return 0;
+        int w = (sum+target)/2;
+        int n = nums.size();
+        cout<<w;
+        // if(w==0) return 2;
+        return solve(n,w,nums);
+    }
+};
