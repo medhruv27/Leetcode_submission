996636e363385c1004ef2520d1bdbf814365ce20
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-20 18:29:10 +0530

    Time: 43 ms (41.18%), Space: 52.9 MB (14.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,66 +1,75 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& dp, vector<int> coins) {
-        if (i == 0)
-            return INT_MAX - 1;
-        if (j == 0)
-            return 0;
-        if (i == 1) {
-            if (j % coins[0] == 0) {
-                return j / coins[0];
-            } else {
-                return INT_MAX - 1;
+
+    // int solve(int n,int w,vector<int>&nums){
+    //     //base cond.
+    //     if(n==0) return INT_MAX-1;
+    //     if(w==0) return 0;
+    //     if(n==1){
+    //         if(w%nums[0]==0){
+    //             return w/nums[0];
+    //         }else{
+    //             return INT_MAX-1;
+    //         }
+    //     }
+    //     if(nums[n-1]<=w){
+    //         return min(solve(n,w-nums[n-1],nums) + 1,solve(n-1,w,nums));
+    //     }
+    //     else{
+    //         return solve(n-1,w,nums);
+    //     }
+    // }
+    //recursive
+
+    // int dp[1002][10002];
+    // int solve(int n,int w,vector<int>&nums){
+    //     if(n==0) return INT_MAX-1;
+    //     if(w==0) return 0;
+    //     if(n==1){
+    //         if(w%nums[0]==0){
+    //             return w/nums[0];
+    //         }else{
+    //             return INT_MAX-1;
+    //         }
+    //     }
+    //     if(dp[n][w]!=-1) return dp[n][w];
+    //     if(nums[n-1]<=w){
+    //         return dp[n][w] = min(solve(n,w-nums[n-1],nums) + 1,solve(n-1,w,nums));
+    //     }
+    //     else{
+    //         return dp[n][w] = solve(n-1,w,nums);
+    //     }
+    // }
+    //memoization
+
+    int solve(int n,int w,vector<int>&nums){
+        vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+        for(int j=0;j<=w;j++){
+            dp[0][j] = INT_MAX-1;
+            if(j%nums[0]==0 && j!=0){
+                dp[1][j] = j/nums[0];
+            }else{
+                if(j!=0)
+                dp[1][j] = INT_MAX-1;
             }
         }
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (coins[i - 1] <= j) {
-            dp[i][j] = min(solve(i, j - coins[i - 1], dp, coins) + 1, solve(i - 1, j, dp, coins));
-        } else {
-            dp[i][j] = solve(i - 1, j, dp, coins);
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<=w;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = min(dp[i][j-nums[i-1]]+1,dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        return dp[i][j];
+        return dp[n][w];
     }
+    //tabular
+
     int coinChange(vector<int>& coins, int w) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(w + 1, -1));
-        int ans = solve(n, w, dp, coins);
-        if (ans >= INT_MAX - 1) {
-            return -1;
-        }
-        return ans;
-
-        // for(int i=0;i<n+1;i++){
-        //     for(int j=0;j<w+1;j++){
-        //         if(j==0){
-        //             dp[i][j] =0;
-        //         }
-        //         if(i==0 ){
-        //             dp[i][j]=INT_MAX-1;
-        //         }
-        //     }
-        // }
-        // for(int j=1;j<w+1;j++){
-        //     if(j%coins[0] ==0){
-        //         dp[1][j] = j/coins[0];
-        //     }
-        //     else{
-        //         dp[1][j] = INT_MAX-1;
-        //     }
-        // }
-        // for(int i=2;i<n+1;i++){
-        //     for(int j=1;j<w+1;j++){
-        //         if(coins[i-1] <=j){
-        //             dp[i][j]= min(dp[i][j-coins[i-1]] +1, dp[i-1][j]);
-        //         }
-        //         else{
-        //             dp[i][j] = dp[i-1][j];
-        //         }
-        //     }
-        // }
-        // if(dp[n][w]>= INT_MAX-1){
-        //     return -1;
-        // }
-        //     return dp[n][w];
+        // memset(dp,-1,sizeof(dp));
+        int ans = solve(n,w,coins);
+        return ans==INT_MAX-1?-1:ans;
     }
 };
