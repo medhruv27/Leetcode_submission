4788c4cf449be27f8e80dd00448d6cf51e2be2d4
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-20 18:47:09 +0530

    Time: 91 ms (6.43%), Space: 53.9 MB (18.82%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,29 +1,60 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>&dp,vector<int>coins){
-        if(i==0) return 0;
-        if(j==0) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(coins[i-1]<=j) dp[i][j] = solve(i,j-coins[i-1],dp,coins) + solve(i-1,j,dp,coins);
-        else dp[i][j] = solve(i-1,j,dp,coins);
-        return dp[i][j];
+    // int solve(int n,int w,vector<int>&nums){
+    //     //base cond.
+    //     if(n==0) return 0;
+    //     if(w==0) return 1;
+    //     if(nums[n-1]<=w){
+    //         return (solve(n,w-nums[n-1],nums) + solve(n-1,w,nums));
+    //     }
+    //     else{
+    //         return solve(n-1,w,nums);
+    //     }
+    // }
+    //recursive
+
+    int dp[1002][10002];
+    int solve(int n,int w,vector<int>&nums){
+        if(n==0) return 0;
+        if(w==0) return 1;
+        if(dp[n][w]!=-1) return dp[n][w];
+        if(nums[n-1]<=w){
+            return dp[n][w] = (solve(n,w-nums[n-1],nums) + solve(n-1,w,nums));
+        }
+        else{
+            return dp[n][w] = solve(n-1,w,nums);
+        }
     }
+    // memoization
+
+    // int solve(int n,int w,vector<int>&nums){
+    //     vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+    //     for(int j=0;j<=w;j++){
+    //         dp[0][j] = INT_MAX-1;
+    //         if(j%nums[0]==0 && j!=0){
+    //             dp[1][j] = j/nums[0];
+    //         }else{
+    //             if(j!=0)
+    //             dp[1][j] = INT_MAX-1;
+    //         }
+    //     }
+    //     for(int i=2;i<=n;i++){
+    //         for(int j=0;j<=w;j++){
+    //             if(nums[i-1]<=j){
+    //                 dp[i][j] = min(dp[i][j-nums[i-1]]+1,dp[i-1][j]);
+    //             }else{
+    //                 dp[i][j] = dp[i-1][j];
+    //             }
+    //         }
+    //     }
+    //     return dp[n][w];
+    // }
+    //tabular
+
     int change(int w, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
-        return solve(n,w,dp,coins);
-        for(int i=0;i<n+1;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<w+1;j++){
-               if(coins[i-1]<=j){
-                   dp[i][j] = (dp[i][j-coins[i-1]]+dp[i-1][j]);
-               }else{
-                   dp[i][j] = dp[i-1][j];
-               }
-            }
-        }
-        return dp[n][w];
+        memset(dp,-1,sizeof(dp));
+        int ans = solve(n,w,coins);
+        return ans==INT_MAX-1?-1:ans;
     }
 };
