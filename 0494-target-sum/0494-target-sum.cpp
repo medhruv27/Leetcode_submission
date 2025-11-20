09914a09fb836933a948532db3d8cb15a8d04bce
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-20 16:10:11 +0530

    Time: 3 ms (87.68%), Space: 15.6 MB (45.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
+    // int solve(int n,int w,vector<int>&nums){
+    //     if(w==0 && n==0) return 1;
+    //     if(n==0) return 0;
+    //     if(nums[n-1]<=w){
+    //         return solve(n-1,w-nums[n-1],nums) + solve(n-1,w,nums);
+    //     }
+    //     else{
+    //         return solve(n-1,w,nums);
+    //     }
+    // }
     int solve(int n,int w,vector<int>&nums){
-        if(w==0 && n==0) return 1;
-        if(n==0) return 0;
-        if(nums[n-1]<=w){
-            return solve(n-1,w-nums[n-1],nums) + solve(n-1,w,nums);
+        vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+        dp[0][0] =1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=w;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        else{
-            return solve(n-1,w,nums);
-        }
+        return dp[n][w];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum  = accumulate(nums.begin(),nums.end(),0);
