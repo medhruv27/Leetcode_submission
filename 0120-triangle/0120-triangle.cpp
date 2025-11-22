48bde16e692410864f358c509c9bf38e5ff08842
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-22 16:54:28 +0530

    Time: 0 ms (100%), Space: 13.3 MB (34.73%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -7,7 +7,7 @@ public:
         if(i==m-1){
             return grid[i][j] ;
         }
-        if(dp[i][j]!=-1){
+        if(dp[i][j]!=INT_MIN){
             return dp[i][j];
         }
        return dp[i][j] =  grid[i][j] + min(solve(i+1,j,m,grid,dp),solve(i+1,j+1,m,grid,dp));
@@ -15,7 +15,7 @@ public:
     }
     int minimumTotal(vector<vector<int>>& grid) {
         int m = grid.size();
-        vector<vector<int>>dp(m,vector<int>(m,-1));
+        vector<vector<int>>dp(m,vector<int>(m,INT_MIN));
         return  solve(0,0,m,grid,dp) ;
     }
 };
