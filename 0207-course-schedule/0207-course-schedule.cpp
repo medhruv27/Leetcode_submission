bb750af57bc28c96715388039018094ec9cf1dd7
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-23 20:41:58 +0530

    Time: 7 ms (48.63%), Space: 19.5 MB (52.51%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& prerequisites) {
-        vector<int> adj[V];
-        for (auto it : prerequisites) {
+    bool canFinish(int v, vector<vector<int>>& pre) {
+        vector<int>adj[v];
+        for(auto it : pre){
             adj[it[0]].push_back(it[1]);
         }
-            vector<int>indeg(V,0);
-            vector<int> topo;
-            for (int i = 0; i < V; i++) {
-                for (auto it : adj[i]) {
-                    indeg[it]++;
-                }
+        vector<int>topo;
+        vector<int>indeg(v,0);
+        for(int i=0;i<v;i++){
+            for(auto it : adj[i]){
+                indeg[it]++;
             }
-            queue<int> q;
-            for (int i = 0; i < V; i++) {
-                if (indeg[i] == 0) {
-                    q.push(i);
-                }
-            }
-            int c =0;
-            while (!q.empty()) {
-                int node = q.front();
-                q.pop();
-                // topo.push_back(node);
-                c++;
-                for (auto it : adj[node]) {
-                    indeg[it]--;
-                    if (indeg[it] == 0) {
-                        q.push(it);
-                    }
-                }
-            }
-            if(c==V){
-                return true;
-            }
-            return false;
         }
-    };
+        queue<int>q;
+        for(int i=0;i<v;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            // topo.push_back(node);
+            c++;
+            for(auto it : adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(c==v){
+            return true;
+        }
+        return false;
+    }
+};
