cb06b4c747f8f2eb351cb7ad98c4d510aa0b7979
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-23 13:38:03 +0530

    Time: 24 ms (5.15%), Space: 19.5 MB (74.92%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,61 +1,30 @@
-class Disjoint{
-public:
-    vector<int>parent,size,rank; 
-    Disjoint(int n){
-        size.resize(n+1,1);
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i] = i;
-        }
-    }
-    int findpar(int node){
-        if(parent[node]==node) return node;
-        return parent[node] = findpar(parent[node]);
-    }
-    void unionbyrank(int u,int v){
-        int up = findpar(u);
-        int vp = findpar(v);
-        if(up==vp) return ;
-        if(rank[up]<rank[vp]){
-            parent[up] = vp;
-        }else if(rank[up]>rank[vp]){
-            parent[vp] = up;
-        }else{
-            parent[vp] =up;
-            rank[up]++;
-        }
-    }
-    void unionbysize(int u,int v){
-        int up = findpar(u);
-        int vp = findpar(v);
-        if(up==vp) return ;
-        if(size[up]<size[vp]){
-            parent[up] = vp;
-            size[vp]+=size[up];
-        }else{
-            parent[vp] = up;
-            size[up]+=size[vp];
-        }
-    }
-};
 class Solution {
 public:
+void dfs(int node,vector<int>&vis,vector<int> adj[]){
+    vis[node] =1;
+    for(auto it : adj[node]){
+        if(!vis[it]){
+            dfs(it,vis,adj);
+        }
+    }
+}
     int findCircleNum(vector<vector<int>>& isConnected) {
         int v = isConnected.size();
-        Disjoint ds(v);
-        vector<int>adj(v);
-       for(int i=0;i<v;i++){
+        vector<int>adj[v];
+        for(int i=0;i<v;i++){
             for(int j=0;j<v;j++){
-                if(isConnected[i][j]==1){
-                    ds.unionbysize(i,j);
+                if(isConnected[i][j]==1 && i!=j){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 } 
             }
         }
+        vector<int>vis(v+1,0);
         int cnt =0;
         for(int i=0;i<v;i++){
-            if(i == ds.parent[i]){
+            if(!vis[i]){
                 cnt++;
+                dfs(i,vis,adj);
             }
         }
         return cnt;
