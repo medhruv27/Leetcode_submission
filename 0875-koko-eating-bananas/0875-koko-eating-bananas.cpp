6b01a85bd63ae898481abc9e64c24eac5ed06d2e
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-11-24 13:25:28 +0530

    Time: 11 ms (41.61%), Space: 23 MB (44.59%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool solve(vector<int> &arr, int k,int mid){
+        int c =0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=mid){
+                c++;
+            }else{
+                int r = ceil((double)arr[i]/mid);
+                c+=r;
+                if(c>k) return false;
+            }
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& arr, int h) {
+        int n = arr.size();
+        int s = 1;
+        int e = *max_element(arr.begin(),arr.end());
+        sort(arr.begin(),arr.end());
+        int res =-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            // cout<<mid;
+            if(solve(arr,h,mid)){
+                res = mid;
+                e = mid-1;
+            }else{
+                s = mid +1;
+            }
+        }
+        return res;
+    }
+};
