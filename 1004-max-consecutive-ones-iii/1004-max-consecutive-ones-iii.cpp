b59deb1b1387bc241b62f9a799038f7d2bd34b94
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-12-01 19:58:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,26 +5,22 @@ public:
         int e =0;
         int n = nums.size();
         int maxi=0;
-        int ans = 0;
-        int cntk = k;
         while(e<n){
             if(nums[e]==0){
-                cntk--;
-            }else{
-                ans++;
+                k--;
             }
-            if(cntk>0){
+            if(kk>0){
                 maxi = max(maxi,e-s+1);
                 e++;
             }
-            else if(cntk==0){
+            else if(k==0){
                 maxi = max(maxi,e-s+1);
                 e++;
             }
             else{
-                while(cntk<0){
+                while(k<0){
                     if(nums[s]==0){
-                        cntk++;
+                        k++;
                     }
                     s++;
                 }
