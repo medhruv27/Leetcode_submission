b4b5b6ba579a051dba46093977323ccfd28464ae
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2025-12-24 13:20:09 +0530

    Time: 8 ms (9.07%), Space: 32.8 MB (98.47%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,41 +1,18 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private:
-    TreeNode* helper(vector<int>& nums, int left, int right) {
-        if (left > right) return nullptr;
-        int mid = left + (right - left) / 2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = helper(nums, left, mid - 1);
-        root->right = helper(nums, mid + 1, right);
-        return root;
-    }
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        vector<int>nums;
-        while(head){
-            nums.push_back(head->val);
-            head=head->next;
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return new TreeNode(head->val);
+        ListNode* slow = head, *fast = head, *slow_prev = nullptr;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow_prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return helper(nums, 0, nums.size() - 1);
+        TreeNode* root = new TreeNode(slow->val);
+        slow_prev->next = nullptr;
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+        return root;
     }
 };
