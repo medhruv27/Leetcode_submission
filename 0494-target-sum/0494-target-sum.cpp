489873ff02b7162b9c30431b09abe80f1d549d0d
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2026-01-28 19:23:26 +0530

    Time: 2 ms (88.2%), Space: 15.9 MB (37.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -10,6 +10,22 @@ public:
     //         return solve(n-1,w,nums);
     //     }
     // }
+    // Recursive
+
+    // int dp[1002][1002];
+    // int solve(int n,int w,vector<int>&nums){
+    //     if(w==0 && n==0) return 1;
+    //     if(n==0) return 0;
+    //     if(dp[n][w]!=-1) return dp[n][w];
+    //     if(nums[n-1]<=w){
+    //         return dp[n][w] = solve(n-1,w-nums[n-1],nums) + solve(n-1,w,nums);
+    //     }
+    //     else{
+    //         return dp[n][w] = solve(n-1,w,nums);
+    //     }
+    // }
+    //memoization
+
     int solve(int n,int w,vector<int>&nums){
         vector<vector<int>>dp(n+1,vector<int>(w+1,0));
         dp[0][0] =1;
@@ -24,13 +40,15 @@ public:
         }
         return dp[n][w];
     }
+    //Tabular
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum  = accumulate(nums.begin(),nums.end(),0);
         target = abs(target);
-        if (sum < target || (target + sum) % 2 != 0) return 0;
+        if ((target + sum) % 2 != 0) return 0;
         int w = (sum+target)/2;
         int n = nums.size();
         cout<<w;
+        // memset(dp,-1,sizeof(dp));
         return solve(n,w,nums);
     }
 };
