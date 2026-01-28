bbf524e2634bdf436c990352354ab8d2e1fa0ea1
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2026-01-28 18:29:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    // bool ss(vector<int>nums , int sum , int n){
-    //     if(sum == 0) return true; 
-    //     if(n == 0 ) return false;
-    //     if(sum >= nums[n-1]){
-    //         return (ss(nums , sum - nums[n-1] , n-1) || ss(nums , sum , n-1));
-    //     }
-    //     else{
-    //         return ss(nums , sum , n-1);
-    //     }
-    // }
-    bool ss(vector<int>&nums,int w,int n){
-        vector<vector<int>>dp(n+1,vector<int>(w+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
+    bool ss(vector<int>&nums,int n,int w){
+        if(n==0){
+            return (w==0)?1:0;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=w;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+        if(w==0){
+            return 1;
         }
-        return dp[n][w];
+        if(nums[n-1]<=w){// can take 
+            return (ss(nums,n-1,w-nums[n-1]) || ss(nums,n-1,w))?1:0;
+        }else{
+            return ss(nums,n-1,w);
+        }
     }
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum % 2 != 0) return false;
-        return ss(nums , sum/2 , nums.size());
+        return ss(nums , nums.size(), sum/2);
     }
 };
