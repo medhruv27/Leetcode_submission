8ead3ad2d016d79d82329a266e6b13b3bd1ebd74
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2026-01-29 18:15:16 +0530

    Time: 22 ms (46.48%), Space: 18.6 MB (39.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,24 @@
 class Solution {
-public:
-    int dp[1001][1001];
-    int maxi =0,indx=-1;
-    string solve(int n,int m,string &s,string &t){
-        for(int i=0;i<=n;i++){
-            dp[i][0] =0;
-            dp[0][i] =0;
+public: 
+    string bothside(string &s,int left,int right){
+        while(left>=0 && right<s.length() && s[left]==s[right]){
+            left--;
+            right++;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    // if(maxi<dp[i][j]){
-                    //     maxi = dp[i][j];
-                    //     indx = i-1;
-                    // }
-                    if (maxi < dp[i][j]) {
-                        if (i - dp[i][j] == n - j) {
-                            maxi = dp[i][j];
-                            indx = i - 1;
-                        }
-                    }
-                }else{
-                    dp[i][j] =0;
-                }
-            }
-        }
-        cout<<maxi<<" "<<indx;
-        return s.substr(indx-maxi+1,maxi);
+        return s.substr(left + 1,right-left-1);
     }
     string longestPalindrome(string s) {
-        string t = s ;
-        int n = s.length();
-        int m = n;
-        reverse(t.begin(),t.end());
-        memset(dp,-1,sizeof(dp));
-        return solve(n,m,s,t);
+        string ans;
+        for(int i=0;i<s.length();i++){
+            string oddpali = bothside(s,i,i);
+            string evenpali = bothside(s,i,i+1);
+            if(oddpali.length()>ans.length()){
+                ans = oddpali;
+            }
+            if(evenpali.length()>ans.length()){
+                ans = evenpali;
+            }
+        }
+        return ans;
     }
 };
