edcacd509b90ed7ff90bcb08e891b558b9602252
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2026-01-30 19:23:04 +0530

    Time: 0 ms (100%), Space: 22.7 MB (5.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& p) {
-        int mini = p[0];
-        int ans=0;
-        for(int i=1;i<p.size();i++){
-            if(p[i]>mini){
-                ans+= p[i]-mini;
-            }
-                mini = p[i];
-            
+    int dp[30001][3];
+    // int solve(int i,int a,int n,vector<int>&p){
+    //     //recursive soln
+    //     // base condn
+    //     if(i==n){
+    //         return 0;
+    //     }
+    //     if(a==1){
+    //         //buy it 
+    //         return max(-p[i] + solve(i+1,0,n,p),0 + solve(i+1,1,n,p));
+    //     }else{
+    //         return max(p[i] + solve(i+1,1,n,p),0 + solve(i+1,0,n,p));
+    //     }
+    // }
+
+    int solve(int i,int a,int n,vector<int>&p){
+        //memo soln
+        // base condn
+        if(i==n){
+            return 0;
         }
-        return ans;
+        if(dp[i][a]!=-1){
+            return dp[i][a];
+        }
+        if(a==1){
+            //buy it 
+            return dp[i][a] = max(-p[i] + solve(i+1,0,n,p),0 + solve(i+1,1,n,p));
+        }else{
+            return dp[i][a] = max(p[i] + solve(i+1,1,n,p),0 + solve(i+1,0,n,p));
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,1,n,prices);
     }
 };
