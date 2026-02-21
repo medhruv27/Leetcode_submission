6c456889dce1589416420e60207c1f1d58eea5a6
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2026-02-22 00:10:36 +0530

    Time: 2 ms (73.15%), Space: 15.1 MB (12.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,13 +4,12 @@ public:
         map<int,int>mp;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            int rem = w - nums[i];
+            int rem  = w - nums[i];
             if(mp.find(rem)!=mp.end()){
-                //exist
                 return {i,mp[rem]};
             }
             mp[nums[i]] = i;
         }
         return {};
     }
-};
+};  
